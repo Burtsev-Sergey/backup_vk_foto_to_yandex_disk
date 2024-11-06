81bde426aabd48492f2f914cc2556592b999d122
Burtsev Sergey <sburtsevs@mail.ru>
2024-11-06 16:40:36 +0300

    Add uploaded_fotos.json file, uploading photos to yandex.disk

--- backup_vk_foto.py
+++ backup_vk_foto.py
@@ -2,45 +2,45 @@ from dotenv import load_dotenv
 import os
 import requests
 from pprint import pprint
+from datetime import datetime, timezone
+import json
 
 # Извлечение из глобальных переменных в операционную систему токенов ВК и Яндекс Диск
 dotenv_path = 'config_example.env'
 if os.path.exists(dotenv_path):
-    load_dotenv(dotenv_path)
+  load_dotenv(dotenv_path)
+
+# Токен Яндекс Диска можно вводить или читать из глобальной переменной
+user_id = input('Введите id пользователя ВК: ')
+# ya_token = input('Введите токен Яндекс Диска: ')
 vk_token = os.getenv('VK_TOKEN')
 ya_token = os.getenv('YA_TOKEN')
 
-
 # Создание класса для извлечения фотографий профиля из аккаунта ВК
 class VKConnektor:
-    def __init__(self, access_token, version='5.199'):
-        self.access_token = access_token
-        self.version = version
-        self.base_url = 'https://api.vk.com/method/'
-        self.params = {
-            'access_token': self.access_token,
-            'v': self.version
-        }
+  def __init__(self, access_token, version='5.199'):
+    self.access_token = access_token
+    self.version = version
+    self.base_url = 'https://api.vk.com/method/'
+    self.params = {
+      'access_token': self.access_token,
+      'v': self.version
+    }
 
-# Метод класса для извлечения фотографий профиля из аккаунта ВК
-    def photo_info(self, user_id):
-        url = f'{self.base_url}photos.get'
-        params = {
-          **self.params,
-          'owner_id': user_id,
-          'album_id': 'profile',
-          'extended': '1'
-        }
-        response = requests.get(url, params=params)
-        return response.json()
+# Метод класса для извлечения фотографий профиля из аккаунта ВК  
+  def photo_info(self, user_id):
+    url = f'{self.base_url}photos.get'
+    params = {
+      **self.params,
+      'owner_id': user_id,
+      'album_id': 'profile',
+      'extended': '1'
+    }
+    response = requests.get(url, params=params)
+    return response.json()
 
 
-# Создание объекта класса для VKConnektor, передача в класс параметров 
-# connector = VKConnektor(vk_token)     
-# photos_info = connector.photo_info(581197663)
-# pprint(photos_info)
-
-# Создание папки для хранения фотографий на Яндекс Диск
+# Класс - создание папки для хранения фотографий на Яндекс Диск
 class YDConnektor:
     def __init__(self, token):
         self.headers = {'Authorization': f'OAuth {token}'}
@@ -51,21 +51,80 @@ class YDConnektor:
                                 params={'path': folder_name})
         return response.status_code
 
+
+# Функция - проверка: ответ от ВК без ошибок, в аккаунте ВК есть фотографии профиля 
+def validate_photo_info(photo_info, user_id):
+  if 'error' in photo_info:
+    print("Проверьте токен ВК!")
+    return False
+
+  photos = photo_info.get('response', {}).get('items', [])
+  if not photos:
+    print(f"У id {user_id} нет фотографий профиля.")
+    return False
+
+  return True
+
+# Функция выделяет из json-файла ответа от ВК 5 фотографий максимального размера в пикселях
+def get_largest_photos(photos_info, top_n=5):
+  photos = photos_info.get('response', {}).get('items', [])
+  largest_photos = []
+
+  for photo in photos:
+    max_size = max(photo['sizes'], key=lambda s: s['width'] * s['height'])
+    largest_photos.append({
+      'url': max_size['url'],
+      'likes': photo['likes']['count'],
+      'date': photo['date'],
+      'area': max_size['width'] * max_size['height']
+    })
+
+  largest_photos.sort(key=lambda p: (p['area'], p['likes'], p['date']), reverse=True)
+  return largest_photos[:top_n]
+
+# Функция загружает фотографии на Яндекс Диск с проверкой прошла загрузка или нет
+def download_photo_to_yandex_disk(photo_info, ya_token):
+  base_url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
+  headers = {
+    'Authorization': f'OAuth {ya_token}'
+  }
+  uploaded_photos_info = []
+
+  for photo in photo_info:
+    likes = photo['likes']
+    date = photo['date']
+  
+    date_str = datetime.fromtimestamp(date, timezone.utc).strftime('%d.%m.%Y')
+
+    filename = f"{likes}_{date_str}.jpg"
+    params = {
+      'path': f'/Image_vk/{filename}',
+      'url': photo['url']
+    }
+
+    response = requests.post(base_url, params=params, headers=headers)
+    if response.status_code == 202:
+      print(f"Фото {filename} успешно загружено на Яндекс Диск")
+      uploaded_photos_info.append({"file_name": filename, "size": f"{photo['area']} px"})
+    else:
+      print(f"Ошибка при загрузке фото на Яндекс Диск {filename}: {response.json()}")
+
+  # Сохранение информации о загруженных на Яндекс Диск фотографиях в JSON-файл
+  with open('uploaded_photos.json', 'w') as json_file:
+    json.dump(uploaded_photos_info, json_file, indent=4)
+  # pprint(uploaded_photos_info)
+
+
+# Создание объекта класса для VKConnektor, передача в класс параметров          
+connector = VKConnektor(vk_token)
+photos_info = connector.photo_info(user_id)
+
 # Запуск создания папки на Яндекс Диск с вводом имени папки    
 # yd_connector = YDConnektor(ya_token)
-new_folder_name = input('Введите название папки на Яндекс Диск латинскими буквами: ')
+# new_folder_name = input('Введите название папки на Яндекс Диск латинскими буквами: ')
 # yd_connector.create_folder({new_folder_name})
 
-# Сохранение фотографии с заданным url в папку на Яндекс Диск
-photo_url = 'https://sun9-80.userapi.com/s/v1/ig2/jRuAvhhXuT2nXa3Z3EeiyCmFPzXBsrvb7idbLrdqR_cQolm3El909wOC0KI9SkbBHkzQyFmqM2hwKkPTRQwxZDOM.jpg?quality=95&as=32x32,48x48,72x72,108x108,160x160,240x240,360x360,480x480,540x540,640x640,720x720,1000x1000&from=bu&cs=1000x1000'
-url = 'https://cloud-api.yandex.net/v1/disk/resources/upload'
-params = {
-    'path': f'/{new_folder_name}/PTRQwxZDOM.jpg',
-    'url': photo_url
-}
-headers = {
-    'Authorization': f'OAuth {ya_token}'
-}
-respons = requests.post(url, params=params, headers=headers)
-print(respons.status_code)
-pprint(respons.json())
+# Запуск загрузки фото на Яндекс Диск в случае когда нет ошибок и есть фотографии
+if validate_photo_info(photos_info, user_id):
+    largest_photos = get_largest_photos(photos_info)
+    download_photo_to_yandex_disk(largest_photos, ya_token)
